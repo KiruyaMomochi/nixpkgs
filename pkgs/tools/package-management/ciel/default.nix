d113feb38d85b1ce0a4f16d05909449cbf57ae3c
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2023-03-13 00:41:44 +0800

    ciel: narrow platforms to linux

--- pkgs/tools/package-management/ciel/default.nix
+++ pkgs/tools/package-management/ciel/default.nix
@@ -52,7 +52,7 @@ rustPlatform.buildRustPackage rec {
     description = "A tool for controlling AOSC OS packaging environments using multi-layer filesystems and containers.";
     homepage = "https://github.com/AOSC-Dev/ciel-rs";
     license = licenses.mit;
-    platforms = platforms.unix;
+    platforms = platforms.linux;
     maintainers = with maintainers; [ yisuidenghua ];
   };
 }
