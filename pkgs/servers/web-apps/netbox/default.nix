decc381187418a12d3ce8a9294a777da1f509bd5
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2023-03-22 16:03:01 +0800

    netbox: patch

--- pkgs/servers/web-apps/netbox/default.nix
+++ pkgs/servers/web-apps/netbox/default.nix
@@ -5,106 +5,118 @@
 , nixosTests
 , python3
 
-, plugins ? ps: [] }:
+, plugins ? ps: [ ]
+}:
 
 let
   py = python3.override {
     packageOverrides = self: super: {
       django = super.django_4;
+      social-auth-app-django = super.social-auth-app-django.overrideAttrs (
+        oldAttrs: rec {
+          version = "5.0.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "python-social-auth";
+            repo = "social-app-django";
+            hash = "sha256-ONhdXxclHRpVtijpKEZlmGDhjid/jnTaPq6LQtjxCC4=";
+            rev = "refs/tags/${version}";
+          };
+        }
+      );
     };
   };
 
   extraBuildInputs = plugins py.pkgs;
 in
 py.pkgs.buildPythonApplication rec {
-    pname = "netbox";
-    version = "3.4.6";
+  pname = "netbox";
+  version = "3.4.6";
 
-    format = "other";
+  format = "other";
 
-    src = fetchFromGitHub {
-      owner = "netbox-community";
-      repo = pname;
-      rev = "refs/tags/v${version}";
-      hash = "sha256-j2Iwh+yGp0xMnZ+Fh7kepb5xzT3GRNhaAiztITgPuws=";
+  src = fetchFromGitHub {
+    owner = "netbox-community";
+    repo = pname;
+    rev = "refs/tags/v${version}";
+    hash = "sha256-j2Iwh+yGp0xMnZ+Fh7kepb5xzT3GRNhaAiztITgPuws=";
+  };
+
+  patches = [
+    # Allow setting the STATIC_ROOT from within the configuration and setting a custom redis URL
+    ./config.patch
+  ];
+
+  propagatedBuildInputs = with py.pkgs; [
+    bleach
+    django_4
+    django-cors-headers
+    django-debug-toolbar
+    django-filter
+    django-graphiql-debug-toolbar
+    django-mptt
+    django-pglocks
+    django-prometheus
+    django-redis
+    django-rq
+    django-tables2
+    django-taggit
+    django-timezone-field
+    djangorestframework
+    drf-yasg
+    swagger-spec-validator # from drf-yasg[validation]
+    graphene-django
+    jinja2
+    markdown
+    markdown-include
+    netaddr
+    pillow
+    psycopg2
+    pyyaml
+    sentry-sdk
+    social-auth-core
+    social-auth-app-django
+    svgwrite
+    tablib
+    jsonschema
+  ] ++ extraBuildInputs;
+
+  buildInputs = with py.pkgs; [
+    mkdocs-material
+    mkdocs-material-extensions
+    mkdocstrings
+    mkdocstrings-python
+  ];
+
+  nativeBuildInputs = [
+    py.pkgs.mkdocs
+  ];
+
+  postBuild = ''
+    PYTHONPATH=$PYTHONPATH:netbox/
+    python -m mkdocs build
+  '';
+
+  installPhase = ''
+    mkdir -p $out/opt/netbox
+    cp -r . $out/opt/netbox
+    chmod +x $out/opt/netbox/netbox/manage.py
+    makeWrapper $out/opt/netbox/netbox/manage.py $out/bin/netbox \
+      --prefix PYTHONPATH : "$PYTHONPATH"
+  '';
+
+  passthru = {
+    # PYTHONPATH of all dependencies used by the package
+    pythonPath = python3.pkgs.makePythonPath propagatedBuildInputs;
+
+    tests = {
+      inherit (nixosTests) netbox;
     };
+  };
 
-    patches = [
-      # Allow setting the STATIC_ROOT from within the configuration and setting a custom redis URL
-      ./config.patch
-    ];
-
-    propagatedBuildInputs = with py.pkgs; [
-      bleach
-      django_4
-      django-cors-headers
-      django-debug-toolbar
-      django-filter
-      django-graphiql-debug-toolbar
-      django-mptt
-      django-pglocks
-      django-prometheus
-      django-redis
-      django-rq
-      django-tables2
-      django-taggit
-      django-timezone-field
-      djangorestframework
-      drf-yasg
-      swagger-spec-validator # from drf-yasg[validation]
-      graphene-django
-      jinja2
-      markdown
-      markdown-include
-      netaddr
-      pillow
-      psycopg2
-      pyyaml
-      sentry-sdk
-      social-auth-core
-      social-auth-app-django
-      svgwrite
-      tablib
-      jsonschema
-    ] ++ extraBuildInputs;
-
-    buildInputs = with py.pkgs; [
-      mkdocs-material
-      mkdocs-material-extensions
-      mkdocstrings
-      mkdocstrings-python
-    ];
-
-    nativeBuildInputs = [
-      py.pkgs.mkdocs
-    ];
-
-    postBuild = ''
-      PYTHONPATH=$PYTHONPATH:netbox/
-      python -m mkdocs build
-    '';
-
-    installPhase = ''
-      mkdir -p $out/opt/netbox
-      cp -r . $out/opt/netbox
-      chmod +x $out/opt/netbox/netbox/manage.py
-      makeWrapper $out/opt/netbox/netbox/manage.py $out/bin/netbox \
-        --prefix PYTHONPATH : "$PYTHONPATH"
-    '';
-
-    passthru = {
-      # PYTHONPATH of all dependencies used by the package
-      pythonPath = python3.pkgs.makePythonPath propagatedBuildInputs;
-
-      tests = {
-        inherit (nixosTests) netbox;
-      };
-    };
-
-    meta = with lib; {
-      homepage = "https://github.com/netbox-community/netbox";
-      description = "IP address management (IPAM) and data center infrastructure management (DCIM) tool";
-      license = licenses.asl20;
-      maintainers = with maintainers; [ minijackson n0emis raitobezarius ];
-    };
-  }
+  meta = with lib; {
+    homepage = "https://github.com/netbox-community/netbox";
+    description = "IP address management (IPAM) and data center infrastructure management (DCIM) tool";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ minijackson n0emis raitobezarius ];
+  };
+}
